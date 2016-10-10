48def5a50e94d0accc5c0001a9f6b42081a81690
sandraduarte <sandramduarte83@gmail.com>
2016-10-10 14:50:34 -0400

    installed npms, add gitignore, and js files

---
+++ index.js
@@ -0,0 +1,37 @@
+// REQUEST
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body); // Show the HTML for the Google homepage. 
+  }
+});
+
+
+//TWITTER
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+//SPOTIFY
+var spotify = require('spotify');
+ 
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    // Do something with 'data' 
+});
